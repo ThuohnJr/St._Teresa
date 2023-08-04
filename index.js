c7c72e12dfcd78345ddb3be19b4ab4d76bc637e7
Thuohn Jr <thuohnjr@outlook.com>
2023-08-04 14:24:55 +0300

    adding files to project

---
+++ index.js
@@ -0,0 +1,8 @@
+function goToPage(element){
+	var page = element.
+}
+document.getElementById(candle-btn).onclick = function(){
+	
+}
+
+document.getElementById("melkio-teaching").alert("This is Melkiohs teachings")
